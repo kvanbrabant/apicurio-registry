eee719c07bfe5155559d4cf6e251db90fec3c5a3
kristiaan <kristiaan.vanbrabant@tree53.com>
2023-08-28 07:47:47 +0200

    Add more tests to BigqueyGsonBuilderTest.

--- schema-util/bigquery/src/test/java/io/apicurio/registry/rules/BigqueryGsonBuilderTest.java
+++ schema-util/bigquery/src/test/java/io/apicurio/registry/rules/BigqueryGsonBuilderTest.java
@@ -1,19 +1,45 @@
 package io.apicurio.registry.rules;
 
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.FieldList;
+import com.google.gson.JsonSyntaxException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BigqueryGsonBuilderTest {
 
+    private final BigqueryGsonBuilder gsonBuilder = new BigqueryGsonBuilder();
+
     @Test
     public void testParse() {
-        BigqueryGsonBuilder gsonBuilder = new BigqueryGsonBuilder();
+        FieldList fieldList = gsonBuilder.parseFields("[{\"name\":\"id\",\"type\":\"STRING\",\"mode\":\"NULLABLE\"}]");
+        assertEquals(Field.Mode.NULLABLE, fieldList.get(0).getMode());
+    }
+
+    @Test
+    public void testParse_other_content() {
+        FieldList fieldList = gsonBuilder.parseFields("[{\"name\":\"id\",\"type\":\"STRING\",\"count\":1}]");
+        assertNull(fieldList.get(0).getMode());
+    }
+
+    @Test
+    public void testParse_illegal_argument() {
         try {
             gsonBuilder.parseFields("[{\"name\":\"id\",\"type\":\"STRING\",\"mode\":\"NULLEABLE\"}]");
             fail("Exception expected");
         } catch (IllegalArgumentException e) {
             assertEquals("No enum constant com.google.cloud.bigquery.Field.Mode.NULLEABLE", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testParse_no_json() {
+        try {
+            gsonBuilder.parseFields("phgp'çàh'nmslenb");
+            fail("Exception expected");
+        } catch (JsonSyntaxException e) {
+            assertEquals("java.lang.IllegalStateException: Expected BEGIN_ARRAY but was STRING at line 1 column 1 path $", e.getMessage());
         }
     }
 }
